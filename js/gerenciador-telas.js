983afd5a45f28f286f7657b5e9f5b02939e802f4
Gabryel Fillipe <byelfcsilva@gmail.com>
2026-02-23 20:37:20 -0300

    feat: desenvolvendo tela home e tela de estudantes

--- js/gerenciador-telas.js
+++ js/gerenciador-telas.js
@@ -1,1 +1,34 @@
-'use strict'
+export const createComponent = (tag, classes = [], texto = '', atributos = {}) => {
+    const elemento = document.createElement(tag)
+    
+    if (classes.length > 0) {
+        elemento.classList.add(...classes)
+    }
+    
+    if (texto) {
+        if (texto.includes('<')) {
+            elemento.innerHTML = texto
+        } else {
+            elemento.textContent = texto
+        }
+    }
+    
+    for (const [chave, valor] of Object.entries(atributos)) {
+        elemento.setAttribute(chave, valor)
+    }
+    
+    return elemento
+}
+
+export const renderScreen = (newHeader, newMain) => {
+    const oldHeader = document.querySelector('header')
+    const oldMain = document.querySelector('main')
+    
+    if (oldHeader && newHeader) {
+        oldHeader.replaceWith(newHeader)
+    }
+    
+    if (oldMain && newMain) {
+        oldMain.replaceWith(newMain)
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
-'use strict'
-
-'use strict'
-
-import { getAllCourses } from './api.js'
+import { getAllCourses, getStudentsByCourse } from './api.js'
+import { createComponent, renderScreen } from './gerenciador-telas.js'
 
 const mapaCursos = {
     'DS': {
         icone: 'fa-code',
-        classe: 'ds' 
+        classe: 'ds'
     },
     'RDS': {
         icone: 'fa-network-wired',
@@ -15,18 +12,100 @@ const mapaCursos = {
     }
 }
 
-const createCourseCard = (curso) => {
-    const botao = document.createElement('button')
+const nomeCursos = {
+    'DS': 'Desenvolvimento de sistemas',
+    'RDS': 'Redes de computadores'
+}
+
+const createStudentCard = (aluno) => {
+    const statusClass = aluno.status && aluno.status.toLowerCase() === 'finalizado' ? 'bg-amarelo' : 'bg-azul'
     
+    const card = createComponent('div', ['card-aluno', statusClass])
+    const img = createComponent('img', ['foto-aluno'], '', { src: aluno.foto })
+    const nome = createComponent('h2', ['nome-aluno'], aluno.nome)
+    
+    card.append(img, nome)
+    
+    return card
+}
+
+const createStudentPage = async (siglaCurso) => {
+    const main = createComponent('main', ['main-alunos'])
+
+    const subHeader = createComponent('div', ['sub-header'])
+
+    const filtroContainer = createComponent('div', ['filtro-container'])
+    const selectStatus = createComponent('select', ['filtro-status'])
+    
+    const opTodos = createComponent('option', [], 'Status', { value: 'Todos' })
+    const opFinalizado = createComponent('option', [], 'Finalizado', { value: 'Finalizado' })
+    const opCursando = createComponent('option', [], 'Cursando', { value: 'Cursando' })
+    
+    selectStatus.append(opTodos, opFinalizado, opCursando)
+    filtroContainer.append( selectStatus)
+
+    const legendaContainer = createComponent('div', ['legenda-container'])
+    const tituloLegenda = createComponent('span', [], 'LEGENDA')
+    const iconeCursando = createComponent('div', ['icone-cursando'])
+    const textoCursando = createComponent('span', [], 'Cursando')
+    const iconeFinalizado = createComponent('div', ['icone-finalizado'])
+    const textoFinalizado = createComponent('span', [], 'Finalizado')
+
+    legendaContainer.append(tituloLegenda, iconeCursando, textoCursando, iconeFinalizado, textoFinalizado)
+    subHeader.append(filtroContainer, legendaContainer)
+
+    const tituloCompleto = nomeCursos[siglaCurso.toUpperCase()] || siglaCurso
+    const tituloCurso = createComponent('h1', ['titulo-curso'], tituloCompleto)
+
+    const containerAlunos = createComponent('div', ['container-alunos'])
+
+    let id = 0
+
+    if (siglaCurso === 'DS'){
+        id = 1
+    } else {
+        id = 2
+    }
+
+    const todosAlunos = await getStudentsByCourse(id)
+
+    const renderCards = (filtro) => {
+        containerAlunos.replaceChildren()
+        
+        const alunosFiltrados = filtro === 'Todos' 
+            ? todosAlunos 
+            : todosAlunos.filter(a => a.status && a.status.toLowerCase() === filtro.toLowerCase())
+
+        alunosFiltrados.forEach(aluno => {
+            const card = createStudentCard(aluno)
+            containerAlunos.appendChild(card)
+        })
+    }
+
+    renderCards('Todos')
+
+    selectStatus.addEventListener('change', (evento) => {
+        renderCards(evento.target.value)
+    })
+
+    main.append(subHeader, tituloCurso, containerAlunos)
+
+    return main
+}
+
+const loadStudentPage = async (siglaCurso) => {
+    const header = createHeader('turma')
+    const main = await createStudentPage(siglaCurso)
+    
+    renderScreen(header, main)
+}
+
+const createCourseCard = (curso) => {
     const estilo = mapaCursos[curso.sigla] || mapaCursos['DS']
     
-    botao.classList.add(estilo.classe)
-    
-    const icone = document.createElement('i')
-    icone.classList.add('fa-solid', estilo.icone)
-    
-    const titulo = document.createElement('h1')
-    titulo.textContent = curso.sigla
+    const botao = createComponent('button', [estilo.classe])
+    const icone = createComponent('i', ['fa-solid', estilo.icone])
+    const titulo = createComponent('h1', [], curso.sigla)
     
     botao.append(icone, titulo)
 
@@ -37,20 +116,62 @@ const createCourseCard = (curso) => {
     return botao
 }
 
-const loadCourses = async () => {
-    const container = document.getElementById('container-cursos')
+const createHeader = (page) => {
+    const header = createComponent('header')
+
+    const containerLogo = createComponent('div', ['container-logo'])
+    const logo = createComponent('img', [], '', { src: './img/logo-image.png' })
+    const titulo = createComponent('h3', [], 'Lion <br>School')
+
+    containerLogo.append(logo, titulo)
+
+    const btnSair = createComponent('button', ['btn-header'])
+    btnSair.addEventListener('click', () => {
+        if (page.toLowerCase() !== 'main') {
+            iniciarApp()
+        }
+    })
+
+    const iconSair = createComponent('img', [], '', { src: './img/Vector.png' })
     
-    if (!container) return 
+    const textoBotao = page.toLowerCase() === 'main' ? 'Sair' : 'Voltar'
+    const textSair = createComponent('p', [], textoBotao)
+
+    btnSair.append(iconSair, textSair)
+    header.append(containerLogo, btnSair)
+
+    return header
+}
+
+const createMainPage = async () => {
+    const main = createComponent('main', ['main-home'])
+
+    const infoCentral = createComponent('div', ['informacao-central'])
+    const titulo = createComponent('h1', [], 'Escolha um <span class="destaque">curso</span> <br> para gerenciar')
+    const pcImg = createComponent('img', [], '', { src: './img/devices.png' })
+
+    infoCentral.append(titulo, pcImg)
+
+    const estudante = createComponent('img', ['estudante'], '', { src: './img/estudante.png' })
+    const containerCursos = createComponent('div', ['cursos'])
 
     const cursos = await getAllCourses()
+    
+    cursos.forEach(curso => {
+        const card = createCourseCard(curso)
+        containerCursos.appendChild(card)
+    })
 
-    const cards = cursos.map(createCourseCard)
-    container.replaceChildren(...cards)
+    main.append(infoCentral, estudante, containerCursos)
+
+    return main
 }
 
-const loadStudentPage = (siglaCurso) => {
-    console.log(`O usuário clicou no curso: ${siglaCurso}`)
+const iniciarApp = async () => {
+    const header = createHeader('main')
+    const main = await createMainPage()
+    
+    renderScreen(header, main)
 }
 
-loadCourses()
-
+iniciarApp()
